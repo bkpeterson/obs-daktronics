117761929bf4d3cca43ed8676a1c89b03d29bcca
Bjorn Peterson <76683417+bkpeterson@users.noreply.github.com>
2025-10-30 11:45:08 -0500

    Try again format

--- src/dak-data-utils.hpp
+++ src/dak-data-utils.hpp
@@ -24,6 +24,10 @@ struct DAKTSVData {
 
 class DAKDataUtils {
 public:
+	static std::map<std::string, DAKSportData *> _allSportsData;
+	static std::map<uint32_t, std::vector<DAKFilter *>> _filters;
+	static std::unique_ptr<SerialPort> serial;
+
 	static void populateSportsData();
 	static void clearSportsData();
 	static DAKSportData *getSportData(std::string sport);
@@ -43,11 +47,6 @@ public:
 	static void startSerial(std::string port);
 
 private:
-	static std::string _DAKData[4];
-	static std::map<std::string, DAKSportData *> _allSportsData;
-	static std::map<uint32_t, std::vector<DAKFilter *>> _filters;
-	static std::unique_ptr<SerialPort> serial;
-
 	static void read_csv_field(std::stringstream &ss, std::string &field);
 	static void read_csv_field(std::stringstream &ss, uint32_t &field);
 	static uint32_t readDataLine(std::stringstream &is, DAKTSVData &data);
