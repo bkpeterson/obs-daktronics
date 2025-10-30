2980e8edcc3f1ed8d91800c662c7a0ccd5f9eb57
Bjorn Peterson <76683417+bkpeterson@users.noreply.github.com>
2025-10-30 08:48:54 -0500

    Serial fix again

--- src/dak-data-utils.cpp
+++ src/dak-data-utils.cpp
@@ -179,7 +179,7 @@ void DAKDataUtils::execute_global_tick_logic(void *data, uint32_t width, uint32_
 void DAKDataUtils::startSerial(std::string port)
 {
 	// Open and start reading
-	if (serial->open(port, SerialPort::BAUD_19200, '\n')) {
+	if (serial->open(port, 19200, '\n')) {
 		serial->startReading();
 	}
 }
